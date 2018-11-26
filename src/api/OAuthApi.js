3be7577c275e3d72ec471b739d7c6d82ebc92c52
Luu Truong <truonglv@outlook.com>
2018-11-26 23:54:34 +0700

    Support TFA

--- src/api/OAuthApi.js
+++ src/api/OAuthApi.js
@@ -3,19 +3,27 @@ import { CLIENT_ID, CLIENT_SECRET } from '../Config';
 import { Fetcher } from '../utils/Fetcher';
 
 export default class OAuthApi {
-    static login(username: String, password: String): Promise {
-        const payload = {
-            username: username,
-            password: passwordEncrypter(password),
-            grant_type: 'password',
-            client_id: CLIENT_ID,
-            password_algo: 'aes128'
-        };
+    static login(
+        username: String,
+        password: String,
+        tfa: ?Object,
+        options: ?Object
+    ): Promise {
+        const payload = Object.assign(
+            {
+                username: username,
+                password: passwordEncrypter(password),
+                grant_type: 'password',
+                client_id: CLIENT_ID,
+                password_algo: 'aes128'
+            },
+            tfa
+        );
 
-        return Fetcher.post('oauth/token', payload);
+        return Fetcher.post('oauth/token', payload, options);
     }
 
-    static refresh(oAuthData): Promise {
+    static refresh(oAuthData, options: ?Object): Promise {
         const payload = {
             grant_type: 'refresh_token',
             client_id: CLIENT_ID,
@@ -23,6 +31,6 @@ export default class OAuthApi {
             refresh_token: oAuthData.refreshToken
         };
 
-        return Fetcher.post('oauth/token', payload);
+        return Fetcher.post('oauth/token', payload, options);
     }
 }
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,7 +4,8 @@ import {
     TextInput,
     StyleSheet,
     ActivityIndicator,
-    Alert
+    Alert,
+    Text
 } from 'react-native';
 import Button from '../components/Button';
 import { Token } from '../utils/Token';
@@ -30,10 +31,14 @@ export default class LoginScreen extends React.Component {
             isSubmitting: true
         });
 
-        const { username, password } = this.state.data;
+        const { username, password, code } = this.state.data;
 
         const onFailedLogin = () => {
-            Alert.alert('Cannot log-in', 'Invalid password or email');
+            if (code.length > 0) {
+                Alert.alert('Cannot verify', 'Invalid code provided');
+            } else {
+                Alert.alert('Cannot log-in', 'Invalid password or email');
+            }
 
             setTimeout(() => {
                 this.setState({
@@ -42,18 +47,44 @@ export default class LoginScreen extends React.Component {
             }, 2000);
         };
 
-        OAuthApi.login(username, password)
+        const tfa = {};
+        const { tfaProvider } = this.state;
+        if (tfaProvider) {
+            tfa.code = code;
+
+            if (tfaProvider === 'email') {
+                tfa.tfa_trigger = 1;
+                tfa.tfa_provider = 'email';
+            } else {
+                tfa.tfa_provider = 'totp';
+            }
+        }
+
+        OAuthApi.login(username, password, tfa, { skipDefaultHandler: true })
             .then((response) => {
-                UserApi.get('me', { oauth_token: response.access_token })
-                    .then((user) => {
-                        Token.saveToken(response);
+                const { data, status, headers } = response;
+                if (status === 202) {
+                    this.setState({
+                        tfaProvider: headers['x-api-tfa-providers'],
+                        isSubmitting: false
+                    });
+                } else if (
+                    status === 200 &&
+                    data.hasOwnProperty('access_token')
+                ) {
+                    UserApi.get('me', { oauth_token: data.access_token })
+                        .then((user) => {
+                            Token.saveToken(data);
 
-                        this.props.navigation.navigate('Home');
+                            this.props.navigation.navigate('Home');
 
-                        Visitor.setVisitor(user);
-                        AuthEvent.dispatch(user);
-                    })
-                    .catch(onFailedLogin);
+                            Visitor.setVisitor(user);
+                            AuthEvent.dispatch(user);
+                        })
+                        .catch(onFailedLogin);
+                } else {
+                    onFailedLogin();
+                }
             })
             .catch(onFailedLogin);
     };
@@ -63,7 +94,8 @@ export default class LoginScreen extends React.Component {
 
         this.state = {
             data: {},
-            isSubmitting: false
+            isSubmitting: false,
+            tfaProvider: ''
         };
     }
 
@@ -77,40 +109,91 @@ export default class LoginScreen extends React.Component {
         }));
     }
 
-    _doRenderField(name, placeholder) {
+    _doRenderField(name, placeholder, props) {
         let secureTextEntry = false,
             keyboardType = 'default';
         if (name.indexOf('password') === 0) {
             secureTextEntry = true;
         }
 
+        const { data, isSubmitting } = this.state;
+
         return (
             <TextInput
-                editable={!this.state.isSubmitting}
+                editable={!isSubmitting}
                 style={styles.input}
                 secureTextEntry={secureTextEntry}
+                autoCapitalize={'none'}
+                autoCorrect={false}
                 keyboardType={keyboardType}
                 onChangeText={(text) => this._onChangeText(name, text)}
                 placeholder={placeholder}
+                value={data[[name]]}
+                {...props}
             />
         );
     }
 
     render() {
-        const { data, isSubmitting } = this.state;
+        const { data, isSubmitting, tfaProvider } = this.state;
+
+        let formComponent;
+        const wrap = {
+            alignItems: 'center',
+            paddingLeft: 20,
+            paddingRight: 20
+        };
+
+        if (tfaProvider) {
+            formComponent = (
+                <View style={wrap}>
+                    <Text style={styles.minorHeading}>
+                        Two-step verification required
+                    </Text>
+                    {this._doRenderField('code', 'Verification code', {
+                        keyboardType: 'number-pad',
+                        autoFocus: true
+                    })}
+                    <Button
+                        title={'Verify'}
+                        disabled={
+                            !data.username || !data.password || isSubmitting
+                        }
+                        onPress={this._doLogin}
+                        style={styles.submit}
+                        textStyle={styles.textStyle}>
+                        {isSubmitting && <ActivityIndicator color="white" />}
+                    </Button>
+                </View>
+            );
+        } else {
+            formComponent = (
+                <View style={wrap}>
+                    {this._doRenderField('username', 'Username or Email', {
+                        autoFocus: true
+                    })}
+
+                    {this._doRenderField('password', 'Password')}
+
+                    <Button
+                        title="Login"
+                        disabled={
+                            !data.username || !data.password || isSubmitting
+                        }
+                        onPress={this._doLogin}
+                        style={styles.submit}
+                        textStyle={styles.textStyle}>
+                        {isSubmitting && <ActivityIndicator color="white" />}
+                    </Button>
+                </View>
+            );
+        }
 
         return (
             <View style={styles.container}>
-                {this._doRenderField('username', 'Username or Email')}
-                {this._doRenderField('password', 'Password')}
-                <Button
-                    title="Login"
-                    disabled={!data.username || !data.password}
-                    onPress={this._doLogin}
-                    style={styles.submit}
-                    textStyle={styles.textStyle}>
-                    {isSubmitting && <ActivityIndicator color="white" />}
-                </Button>
+                <Text style={styles.heading}>XenApp</Text>
+
+                {formComponent}
             </View>
         );
     }
@@ -119,18 +202,24 @@ export default class LoginScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingLeft: 20,
-        paddingRight: 20
+        marginTop: '30%'
+    },
+
+    heading: {
+        fontSize: 30,
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+
+    minorHeading: {
+        fontSize: 18,
+        marginBottom: 10
     },
 
     input: {
         width: '100%',
         marginBottom: 10,
         padding: 10,
-
-        color: 'red',
         fontSize: 18,
         backgroundColor: '#FFF',
 
